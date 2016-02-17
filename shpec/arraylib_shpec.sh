c58e1c66b8db0e5cd87224165284ae8f688b1d90
Ted Lilley <ted.lilley@accelerated.com>
2016-02-16 23:31:01 -0500

    array_find

--- shpec/arraylib_shpec.sh
+++ shpec/arraylib_shpec.sh
@@ -2,10 +2,22 @@
 
 source lib/arraylib
 
-describe "the contains function"
+describe "array_contains"
   it "tests for element membership"
     letters=( a b c )
     array_contains "a" "${letters[@]}"
-    assert equal "$?" 0
+    assert equal "${?}" 0
+  end
+end
+
+describe "array_find"
+  it "finds the index of the first element on exact match"
+    letters=( a b c )
+    assert equal "$(array_find "a" "${letters[@]}")" 0
+  end
+
+  it "finds the index of the second element on exact match"
+    letters=( a b c )
+    assert equal "$(array_find "b" "${letters[@]}")" 1
   end
 end
