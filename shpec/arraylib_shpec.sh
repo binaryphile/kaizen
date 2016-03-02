157a488604424fe59f1131848dbcbdf9b0dbf1e4
Ted Lilley <ted.lilley@gmail.com>
2016-03-01 23:46:21 -0500

    ary

--- shpec/arraylib_shpec.sh
+++ shpec/arraylib_shpec.sh
@@ -2,37 +2,49 @@
 
 source lib/arraylib
 
-describe "array_contains"
+describe "ary_join"
+  it "joins array elements"
+    array=( a b c )
+    assert equal "$(ary_join "|" "${array[@]}")" "a|b|c"
+  end
+
+  it "allows a multicharacter delimiter"
+    array=( a b c )
+    assert equal "$(ary_join " | " "${array[@]}")" "a | b | c"
+  end
+end
+
+describe "ary_contains"
   it "tests for element membership"
     letters=( a b c )
-    array_contains "a" "${letters[@]}"
+    ary_contains "a" "${letters[@]}"
     assert equal "${?}" 0
   end
 end
 
-describe "array_find"
+describe "ary_find"
   it "finds the index of the first element on exact match"
     letters=( a b c )
-    assert equal "$(array_find "a" "${letters[@]}")" 0
+    assert equal "$(ary_find "a" "${letters[@]}")" 0
   end
 
   it "finds the index of the second element on exact match"
     letters=( a b c )
-    assert equal "$(array_find "b" "${letters[@]}")" 1
+    assert equal "$(ary_find "b" "${letters[@]}")" 1
   end
 end
 
-describe "array_remove"
+describe "ary_remove"
   it "removes the last element of an array on exact match"
     letters=( a b c )
-    result=( $(array_remove "c" "${letters[@]}") )
+    result=( $(ary_remove "c" "${letters[@]}") )
     target=( a b )
     assert equal "${result[*]}" "${target[*]}"
   end
 
   it "doesn't remove elements which aren't there"
     letters=( a b c )
-    result=( $(array_remove "d" "${letters[@]}") )
+    result=( $(ary_remove "d" "${letters[@]}") )
     target=( a b c )
     assert equal "${result[*]}" "${target[*]}"
   end
--- shpec/stringlib_shpec.sh
+++ shpec/stringlib_shpec.sh
@@ -3,18 +3,6 @@
 
 source lib/stringlib
 
-describe "str_join"
-  it "joins array elements"
-    array=( a b c )
-    assert equal "$(str_join "|" "${array[@]}")" "a|b|c"
-  end
-
-  it "allows a multicharacter delimiter"
-    array=( a b c )
-    assert equal "$(str_join " | " "${array[@]}")" "a | b | c"
-  end
-end
-
 describe "str_split"
   it "splits on the specified delimiter"
     str="a/b/c"
