abcb27c4013f6503b718cc187d0026d1305070a4
Ted Lilley <ted.lilley@gmail.com>
2016-03-12 22:03:04 -0500

    run

--- shpec/stdlib_shpec.sh
+++ shpec/stdlib_shpec.sh
@@ -2,12 +2,3 @@
 # vim: ft=sh
 
 source lib/stdlib
-
-describe "run_in_directory"
-  it "evals properly"
-    sample=( hello "there  you" )
-    target="hello there  you"
-    result=$(run_in_directory "${HOME}" "echo" "${sample[@]}")
-    assert equal "${target}" "${result}"
-  end
-end
