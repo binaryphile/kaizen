7080bd18d511fcfdf005cff6191bb3e7629da733
Ted Lilley <ted.lilley@accelerated.com>
2016-06-15 14:24:46 -0400

    fp

--- shpec/functional_shpec.sh
+++ shpec/functional_shpec.sh
@@ -3,11 +3,11 @@
 # https://stackoverflow.com/questions/192292/bash-how-best-to-include-other-scripts/12694189#12694189
 [[ -d ${BASH_SOURCE%/*} ]] && _shpec_dir="${BASH_SOURCE%/*}" || _shpec_dir="$PWD"
 
-source "${_shpec_dir}/../lib/functional.sh"
+source "$_shpec_dir"/../lib/functional.sh
 
-describe "func.map"
+describe "fp.map"
   it "maps an array"
     array=( "a" "b" "c" )
-    assert equal "$(func.map "printf" "${array[@]}")" "abc"
+    assert equal "$(fp.map "printf" "${array[@]}")" "abc"
   end
 end
