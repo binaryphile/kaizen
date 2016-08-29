1c6facf87e8d6fbd0e4541a178f48143cf65e006
Ted Lilley <ted.lilley@accelerated.com>
2016-08-29 15:39:08 -0400

    quotes

--- lib/shell.sh
+++ lib/shell.sh
@@ -4,7 +4,7 @@
 [[ -z $_bashlib_shell ]] || return 0
 
 # shellcheck disable=SC2046,SC2155
-readonly _bashlib_shell="$(set -- $(sha1sum "$BASH_SOURCE"); printf "%s" "$1")"
+readonly _bashlib_shell=$(set -- $(sha1sum "$BASH_SOURCE"); printf "%s" "$1")
 
 source "${BASH_SOURCE%/*}"/core.sh 2>/dev/null || source core.sh
 
