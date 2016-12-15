6125f8dcbf1af6ac89da263cc2a53c016644536d
Ted Lilley <ted.lilley@gmail.com>
2016-12-15 00:42:12 -0500

    cleanup

--- shpec/kzn_instantiate_shpec.bash
+++ shpec/kzn_instantiate_shpec.bash
@@ -9,8 +9,8 @@ initialize_shpec_helper
 
 describe "instantiate"
   it "works for one argument"; (
-    params=( one )
     set -- 1
+    params=( one )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1" "$result"
     # shellcheck disable=SC2154
@@ -18,54 +18,54 @@ describe "instantiate"
   end
 
   it "works for two arguments"; (
-    params=( one two )
     set -- 1 2
+    params=( one two )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1 two=2" "$result"
     return "$_shpec_failures" )
   end
 
   it "works with one argument from a hash"; (
-    params=( one )
     # shellcheck disable=SC2034
     declare -A args=( [one]=1 )
     set -- :args
+    params=( one )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1" "$result"
     return "$_shpec_failures" )
   end
 
   it "works with one positional argument and one from a hash"; (
-    params=( one two )
     # shellcheck disable=SC2034
     declare -A args=( [two]=2 )
     set -- 1 :args
+    params=( one two )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1 two=2" "$result"
     return "$_shpec_failures" )
   end
 
   it "instantiates empty variables"; (
-    params=( one )
     # shellcheck disable=SC2034
-    set -- ""
+    set -- ''
+    params=( one )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=''" "$result"
     return "$_shpec_failures" )
   end
 
   it "works for an in-line named argument"; (
-    params=( one )
     set -- :one=1
+    params=( one )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1" "$result"
     return "$_shpec_failures" )
   end
 
   it "works for a hash"; (
-    params=( :hash )
     local -A args=( [one]=1 )
     set -- args
+    params=( :hash )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     expected=$(printf 'declare -A hash=%s([one]="1" )%s' "'" "'")
     assert equal "$expected" "$result"
@@ -73,10 +73,10 @@ describe "instantiate"
   end
 
   it "works for two hashes"; (
-    params=( :hash1 :hash2 )
     local -A args1=( [one]=1 )
     local -A args2=( [two]=2 )
     set -- args1 args2
+    params=( :hash1 :hash2 )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     expected=$(printf 'declare -A hash1=%s([one]="1" )%s;declare -A hash2=%s([two]="2" )%s' "'" "'" "'" "'")
     assert equal "$expected" "$result"
@@ -84,9 +84,9 @@ describe "instantiate"
   end
 
   it "works for an argument and a hash"; (
-    params=( one :hash )
     local -A args=( [two]=2 )
     set -- 1 args
+    params=( one :hash )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     expected=$(printf 'declare one=1;declare -A hash=%s([two]="2" )%s' "'" "'")
     assert equal "$expected" "$result"
@@ -94,63 +94,63 @@ describe "instantiate"
   end
 
   it "resolves in favor of positional arguments vs named keywords"; (
-    params=( one )
     # shellcheck disable=SC2034
-    set -- 1 :one=override
+    set -- 1 :one=fail
+    params=( one )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1" "$result"
     return "$_shpec_failures" )
   end
 
   it "resolves in favor of positional arguments vs hashes"; (
-    params=( one )
     # shellcheck disable=SC2034
     declare -A args=( [one]=override )
     set -- 1 :args
+    params=( one )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1" "$result"
     return "$_shpec_failures" )
   end
 
   it "resolves in favor of keyword arguments vs hashes"; (
-    params=( one )
     # shellcheck disable=SC2034
     declare -A args=( [one]=override )
     set -- :one=1 :args
+    params=( one )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1" "$result"
     return "$_shpec_failures" )
   end
 
   it "accepts all three forms"; (
-    params=( one two three )
     # shellcheck disable=SC2034
     declare -A args=( [three]=3 )
     set -- 1 :two=2 :args
+    params=( one two three )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1 two=2 three=3" "$result"
     return "$_shpec_failures" )
   end
 
   it "allows out of order definition in keyword and hash arguments"; (
-    params=( one two )
     # shellcheck disable=SC2034
     declare -A args=( [one]=1 )
     # shellcheck disable=SC2034
     set -- :two=2 :args
+    params=( one two )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1 two=2" "$result"
     return "$_shpec_failures" )
   end
 
   it "resolves multiple overlapping hashes in order"; (
-    params=( one )
     # shellcheck disable=SC2034
     declare -A args1=( [one]=1 )
     # shellcheck disable=SC2034
-    declare -A args2=( [one]=override )
+    declare -A args2=( [one]=fail )
     # shellcheck disable=SC2034
     set -- :args1 :args2
+    params=( one )
     result=$(instantiate "${#params[@]}" "${params[@]}" "$@")
     assert equal "declare one=1" "$result"
     return "$_shpec_failures" )
