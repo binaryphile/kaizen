6d9fb0b6dfd16a2bdfc3fe8762ef2b4affd9a27a
Ted Lilley <ted.lilley@gmail.com>
2016-12-20 00:39:49 -0500

    remove unnecessary eval

--- shpec/kzn_passed_shpec.bash
+++ shpec/kzn_passed_shpec.bash
@@ -10,40 +10,40 @@ initialize_shpec_helper
 describe 'passed'
   it 'creates a scalar declaration from an array naming a single parameter with the value passed after'
     set -- 0
-    args=( zero ) # shellcheck disable=SC2034
-    assert equal 'declare zero=0' "$(eval 'passed args "$@"')"
+    params=( zero ) # shellcheck disable=SC2034
+    assert equal 'declare zero=0' "$(passed params "$@")"
   end
 
   it 'works for two arguments'
     set -- 0 1
-    args=( zero one ) # shellcheck disable=SC2034
-    assert equal 'declare zero=0;declare one=1' "$(eval 'passed args "$@"')"
+    params=( zero one ) # shellcheck disable=SC2034
+    assert equal 'declare zero=0;declare one=1' "$(passed params "$@")"
   end
 
   it 'creates an array declaration from a special syntax'
     values=( zero one ) # shellcheck disable=SC2034
     set -- values
-    args=( @array ) # shellcheck disable=SC2034
-    assert equal 'declare -a array='\''([0]="zero" [1]="one")'\' "$(eval 'passed args "$@"')"
+    params=( @array ) # shellcheck disable=SC2034
+    assert equal 'declare -a array='\''([0]="zero" [1]="one")'\' "$(passed params "$@")"
   end
 
   it 'creates a hash declaration from a special syntax'
     # shellcheck disable=SC2034
     declare -A values=( [zero]=0 [one]=1 )
     set -- values
-    args=( %hash ) # shellcheck disable=SC2034
-    assert equal 'declare -A hash='\''([one]="1" [zero]="0" )'\' "$(eval 'passed args "$@"')"
+    params=( %hash ) # shellcheck disable=SC2034
+    assert equal 'declare -A hash='\''([one]="1" [zero]="0" )'\' "$(passed params "$@")"
   end
 
   it 'accepts an array literal'
     set -- '[0]="zero" [1]="one"'
-    args=( @array ) # shellcheck disable=SC2034
-    assert equal 'declare -a array='\''([0]="zero" [1]="one")'\' "$(eval 'passed args "$@"')"
+    params=( @array ) # shellcheck disable=SC2034
+    assert equal 'declare -a array='\''([0]="zero" [1]="one")'\' "$(passed params "$@")"
   end
 
   it 'accepts a hash literal'
     set -- '[zero]="0" [one]="1"'
-    args=( %hash ) # shellcheck disable=SC2034
-    assert equal 'declare -A hash='\''([zero]="0" [one]="1")'\' "$(eval 'passed args "$@"')"
+    params=( %hash ) # shellcheck disable=SC2034
+    assert equal 'declare -A hash='\''([zero]="0" [one]="1")'\' "$(passed params "$@")"
   end
 end
