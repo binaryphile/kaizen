7fa9857d4f772c50813d20774d38393d318931e2
Ted Lilley <ted.lilley@gmail.com>
2016-12-22 22:41:07 -0500

    trim out deprecated

--- shpec/shpec_helper_shpec.bash
+++ shpec/shpec_helper_shpec.bash
@@ -6,19 +6,20 @@ initialize_shpec_helper
 describe 'cleanup'
   it 'should delete a given directory'
     # shellcheck disable=SC2154
-    tempdir=$($mktempd) || return 1
-    is_directory "$tempdir"
+    dir=$($mktempd) || return 1
+    is_directory "$dir"
     assert equal 0 $?
-    cleanup "$tempdir"
-    is_directory "$tempdir"
+    cleanup "$dir"
+    is_directory "$dir"
     assert unequal 0 $?
   end
 
   it 'should validate before deletion'; (
-    stub_command validate_dirname "printf validate"
-    stub_command rm "puts ' rm'"
+    stub_command validate_dirname 'printf validate'
+    stub_command rm "$(printf 'puts %s rm%s' \' \')"
     result=$(cleanup)
     assert equal 'validate rm' "$result"
+    # shellcheck disable=SC2154
     return "$_shpec_failures" )
   end
 end
@@ -28,8 +29,11 @@ describe 'shpec_source'
   it 'should source a file in the shpec directory'; (
     path=$(absolute_path "$(dirname "$BASH_SOURCE")")
     cd "$path"
+    touch shpec_empty.bash
     shpec_source shpec/shpec_empty.bash
     assert equal 0 $?
+    # shellcheck disable=SC2154
+    $rm shpec_empty.bash
     return "$_shpec_failures" )
   end
 
@@ -37,12 +41,12 @@ describe 'shpec_source'
     path=$(absolute_path "$(dirname "$BASH_SOURCE")")
     dir=$(mktemp -qdp "$path") || return 1
     cd "$dir"
-    touch shpec/shpec_empty.bash
+    touch ../shpec_empty.bash
     # shellcheck disable=SC2154
     shpec_source shpec/shpec_empty.bash
     assert equal 0 $?
     # shellcheck disable=SC2154
-    $rm shpec/shpec_empty.bash
+    $rm ../shpec_empty.bash
     # shellcheck disable=SC2154
     $rm "$dir"
     return "$_shpec_failures" )
