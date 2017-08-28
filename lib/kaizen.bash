c49f3cc9d106622673ff1044a8a31f5f88df75a9
Ted Lilley <ted.lilley@accelerated.com>
2017-08-28 15:03:57 -0400

    feat: given?

--- lib/kaizen.bash
+++ lib/kaizen.bash
@@ -34,7 +34,7 @@ glob () {
 less_than?          () { (( ($# - 1) < $1 ))              ;}
 more_than?          () { (( ($# - 1) > $1 ))              ;}
 nonexecutable_file? () { file? "$1" && ! executable? "$1" ;}
-starts_with?        () { [[ $2 -- $1*     ]]              ;}
+starts_with?        () { [[ $2 == $1*     ]]              ;}
 sourced?            () { [[ ${FUNCNAME[1]} == 'source' ]] ;}
 trim_from_last      () { __=${2%$1*}                      ;}
 trim_to_last        () { __=${2##*$1}                     ;}
