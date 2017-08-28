f6257d26936601e65497bd6ceea43f156a726136
Ted Lilley <ted.lilley@accelerated.com>
2017-08-28 15:02:08 -0400

    feat: given?

--- lib/kaizen.bash
+++ lib/kaizen.bash
@@ -14,13 +14,13 @@ kaizen_init () {
 
 append_to_file      () { put "$2" >>"$1"                  ;}
 args?               () { (( $# ))                         ;}
-contains?           () { [[ $2 == *"$1"* ]]               ;}
-directory?          () { [[ -d $1 ]]                      ;}
-ends_with?          () { [[ ${2:-} == *${1:-} ]]          ;}
-executable?         () { [[ -x $1 ]]                      ;}
-executable_file?    () { file? "$1" &&   executable? "$1" ;}
-file?               () { [[ -f $1       ]]                ;}
-given?              () { [[ -n ${!1:-}  ]]                ;}
+contains?           () { [[ $2 == *"$1"*  ]]              ;}
+directory?          () { [[ -d $1         ]]              ;}
+ends_with?          () { [[ $2 == *$1     ]]              ;}
+executable?         () { [[ -x $1         ]]              ;}
+executable_file?    () { file? "$1" && executable? "$1"   ;}
+file?               () { [[ -f $1         ]]              ;}
+given?              () { [[ -n ${!1:-}    ]]              ;}
 
 glob () {
   local ary=()
@@ -34,6 +34,7 @@ glob () {
 less_than?          () { (( ($# - 1) < $1 ))              ;}
 more_than?          () { (( ($# - 1) > $1 ))              ;}
 nonexecutable_file? () { file? "$1" && ! executable? "$1" ;}
+starts_with?        () { [[ $2 -- $1*     ]]              ;}
 sourced?            () { [[ ${FUNCNAME[1]} == 'source' ]] ;}
 trim_from_last      () { __=${2%$1*}                      ;}
 trim_to_last        () { __=${2##*$1}                     ;}
