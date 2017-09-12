049b4df4ca0997366efdbae21e0105dc7b96ec31
Ted Lilley <ted.lilley@gmail.com>
2017-09-12 09:23:54 +0000

    kzn shpecs

--- shpec/kzn_shpec.bash
+++ shpec/kzn_shpec.bash
@@ -130,7 +130,7 @@ describe defa
       two
       three
     EOS
-    result=$(printf '(%s) ' "${results[@]}")
+    printf -v result '(%s) ' "${results[@]}"
     assert equal '(one) (two) (three)' "${result% }"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
@@ -142,7 +142,7 @@ describe defa
       two
       three
     EOS
-    result=$(printf '(%s) ' "${results[@]}")
+    printf -v result '(%s) ' "${results[@]}"
     assert equal '(one) (two) (three)' "${result% }"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
@@ -206,7 +206,7 @@ describe geta
       one
       two
     EOS
-    result=$(printf '(%s) ' "${results[@]}")
+    printf -v result '(%s) ' "${results[@]}"
     assert equal "(      zero) (      one) (      two)" "${result% }"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
@@ -492,157 +492,156 @@ describe is_given
   end
 end
 
-# describe is_same_as
-#   it "detects equivalent strings"
-#     is_same_as one one
-#     assert equal 0 $?
-#   end
-#
-#   it "detects non-equivalent strings"
-#     stop_on_error off
-#     is_same_as one two
-#     assert unequal 0 $?
-#     stop_on_error
-#   end
-# end
-#
-# describe is_set
-#   it "returns true if a variable is set"
-#     sample=true
-#     is_set sample
-#     assert equal 0 $?
-#   end
-#
-#   it "returns true if a variable is set to empty"
-#     sample=''
-#     is_set sample
-#     assert equal 0 $?
-#   end
-#
-#   it "returns false if a variable is not set"
-#     unset -v sample
-#     stop_on_error off
-#     is_set sample
-#     assert unequal 0 $?
-#     stop_on_error
-#   end
-# end
-#
-# describe is_symlink
-#   it "doesn't identify a file"
-#     dir=$($mktempd)
-#     validate_dirname "$dir" || return
-#     touch "$dir"/file
-#     stop_on_error off
-#     is_symlink "$dir"/file
-#     assert unequal 0 $?
-#     stop_on_error
-#     shpec_cleanup "$dir"
-#   end
-#
-#   it 'identifies a symlink to a file'
-#     dir=$($mktempd)
-#     validate_dirname "$dir" || return
-#     touch "$dir"/file
-#     $ln file "$dir"/filelink
-#     is_symlink "$dir"/filelink
-#     assert equal 0 $?
-#     shpec_cleanup "$dir"
-#   end
-#
-#   it "identifies a symlink to a directory"
-#     dir=$($mktempd)
-#     validate_dirname "$dir" || return
-#     $ln . "$dir"/dirlink
-#     is_symlink "$dir"/dirlink
-#     assert equal 0 $?
-#     shpec_cleanup "$dir"
-#   end
-#
-#   it "doesn't identify a directory"
-#     dir=$($mktempd)
-#     validate_dirname "$dir" || return
-#     stop_on_error off
-#     is_symlink "$dir"
-#     assert unequal 0 $?
-#     stop_on_error
-#     shpec_cleanup "$dir"
-#   end
-# end
-#
-# describe joina
-#   it "joins an array with a delimiter"
-#     declare -a samples=([0]=zero [1]=one)
-#     assert equal 'zero@one' "$(joina '@' samples)"
-#   end
-#
-#   it "joins an array with one item"
-#     declare -a samples=([0]=zero)
-#     assert equal 'zero' "$(joina ';' samples)"
-#   end
-# end
-#
-# describe puts
-#   it "outputs a string on stdout"
-#     assert equal sample "$(puts 'sample')"
-#   end
-# end
-#
-# describe putserr
-#   it "outputs a string on stderr"
-#     assert equal sample "$(putserr 'sample' 2>&1)"
-#   end
-# end
-#
-# describe splits
-#   it "splits a string into an array on a partition character"
-#     results=()
-#     sample='a=b'
-#     splits '=' sample results
-#     printf -v expected 'declare -a results=%s([0]="a" [1]="b")%s' \' \'
-#     assert equal "$expected" "$(declare -p results)"
-#   end
-# end
-#
-# describe starts_with
-#   it "detects if a string starts with a specified character"
-#     starts_with / /test
-#     assert equal 0 $?
-#   end
-#
-#   it "detects if a string doesn't end with a specified character"
-#     stop_on_error off
-#     starts_with / test
-#     assert unequal 0 $?
-#     stop_on_error
-#   end
-# end
-#
-# describe stripa
-#   it "strips each element of an array"
-#     results=("    zero" "    one" "    two")
-#     stripa results
-#     expected=$(printf 'declare -a results=%s([0]="zero" [1]="one" [2]="two")%s' \' \')
-#     assert equal "$expected" "$(declare -p results)"
-#   end
-#
-#   it "leaves a blank element intact"
-#     results=("    zero" "    one"  "" "    three")
-#     stripa results
-#     expected='declare -a results=%s([0]="zero" [1]="one" [2]="" [3]="three")%s'
-#     printf -v expected "$expected" \' \'
-#     assert equal "$expected" "$(declare -p results)"
-#   end
-# end
-#
-# describe to_lower
-#   it "should lower-case a string"
-#     assert equal upper "$(to_lower UPPER)"
-#   end
-# end
-#
-# describe to_upper
-#   it "should upper-case a string"
-#     assert equal LOWER "$(to_upper lower)"
-#   end
-# end
+describe is_same_as
+  it "detects equivalent strings"
+    is_same_as one one
+    assert equal 0 $?
+  end
+
+  it "detects non-equivalent strings"
+    stop_on_error off
+    is_same_as one two
+    assert unequal 0 $?
+    stop_on_error
+  end
+end
+
+describe is_set
+  it "returns true if a variable is set"
+    sample=true
+    is_set sample
+    assert equal 0 $?
+  end
+
+  it "returns true if a variable is set to empty"
+    sample=''
+    is_set sample
+    assert equal 0 $?
+  end
+
+  it "returns false if a variable is not set"
+    unset -v sample
+    stop_on_error off
+    is_set sample
+    assert unequal 0 $?
+    stop_on_error
+  end
+end
+
+describe is_symlink
+  it "doesn't identify a file"
+    dir=$($mktempd)
+    validate_dirname "$dir" || return
+    touch "$dir"/file
+    stop_on_error off
+    is_symlink "$dir"/file
+    assert unequal 0 $?
+    stop_on_error
+    shpec_cleanup "$dir"
+  end
+
+  it 'identifies a symlink to a file'
+    dir=$($mktempd)
+    validate_dirname "$dir" || return
+    touch "$dir"/file
+    $ln file "$dir"/filelink
+    is_symlink "$dir"/filelink
+    assert equal 0 $?
+    shpec_cleanup "$dir"
+  end
+
+  it "identifies a symlink to a directory"
+    dir=$($mktempd)
+    validate_dirname "$dir" || return
+    $ln . "$dir"/dirlink
+    is_symlink "$dir"/dirlink
+    assert equal 0 $?
+    shpec_cleanup "$dir"
+  end
+
+  it "doesn't identify a directory"
+    dir=$($mktempd)
+    validate_dirname "$dir" || return
+    stop_on_error off
+    is_symlink "$dir"
+    assert unequal 0 $?
+    stop_on_error
+    shpec_cleanup "$dir"
+  end
+end
+
+describe joina
+  it "joins an array with a delimiter"
+    declare -a samples=([0]=zero [1]=one)
+    assert equal 'zero@one' "$(joina '@' samples)"
+  end
+
+  it "joins an array with one item"
+    declare -a samples=([0]=zero)
+    assert equal 'zero' "$(joina ';' samples)"
+  end
+end
+
+describe puts
+  it "outputs a string on stdout"
+    assert equal sample "$(puts 'sample')"
+  end
+end
+
+describe putserr
+  it "outputs a string on stderr"
+    assert equal sample "$(putserr 'sample' 2>&1)"
+  end
+end
+
+describe splits
+  it "splits a string into an array on a partition character"
+    results=()
+    sample=a=b
+    splits = sample results
+    printf -v result '(%s) ' "${results[@]}"
+    assert equal '(a) (b)' "${result% }"
+  end
+end
+
+describe starts_with
+  it "detects if a string starts with a specified character"
+    starts_with / /test
+    assert equal 0 $?
+  end
+
+  it "detects if a string doesn't end with a specified character"
+    stop_on_error off
+    starts_with / test
+    assert unequal 0 $?
+    stop_on_error
+  end
+end
+
+describe stripa
+  it "strips each element of an array"
+    results=("    zero" "    one" "    two")
+    stripa results
+    printf -v result '(%s) ' "${results[@]}"
+    assert equal '(zero) (one) (two)' "${result% }"
+  end
+
+  it "leaves a blank element intact"
+    results=("    zero" "    one"  "" "    three")
+    stripa results
+    printf -v result '(%s) ' "${results[@]}"
+    assert equal '(zero) (one) () (three)' "${result% }"
+  end
+end
+
+describe to_lower
+  it "lower-cases a string"
+    assert equal upper "$(to_lower UPPER)"
+  end
+end
+
+describe to_upper
+  it "upper-cases a string"
+    assert equal LOWER "$(to_upper lower)"
+  end
+end
